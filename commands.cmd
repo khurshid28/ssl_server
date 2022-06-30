b08b32655c7b44675baa14da34c897ba5837f6e5
khurshid <95979088+khurshid28@users.noreply.github.com>
2022-06-30 11:24:12 -0700

    Update commands.cmd

--- commands.cmd
+++ commands.cmd
@@ -1,9 +1,26 @@
-mkdir server
-cd server
+start chrome https://www.filehorse.com/download-openssl-64/download/
+
+mkdir server 
+
+cd server 
 
 git clone https://github.com/khurshid28/ssl_server.git
 
 cd ssl_server
+
+rmdir cert
+
+mkdir cert
+
+cd cert 
+
+openssl  genrsa -out key.pem 
+
+openssl req -new -key key.pem -out csr.pem 
+
+openssl x509 -req -days 9999 -in csr.pem -signkey key.pem -out cert.pem
+
+cd ..
 
 npm i --save
 
